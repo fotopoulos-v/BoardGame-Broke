91d074e2faba02f13c6113a82713f5fbf70efe86
fotopoulos-v <fotopoulos.v2@gmail.com>
2026-04-20 01:23:04 +0300

    trying a new session id

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 FIRECRAWL_API_KEY = "fc-ecc86088dc5e42a3aa4ea1a9fe69e0c9"
 # EFANTASY_SESSION_ID = "r7VBVEoumtmie0ZGYWAjwcc1d5raIUq"
-EFANTASY_SESSION_ID = "zfGlsnEZ2JcLEjjF5lZseR0z-Q6RS5d8"
+EFANTASY_SESSION_ID = "aCCrDbhXVEVnSP9o0fB6720KdLTtFNNk"
 
 # Optional overrides for Public Findbar API search.
 # If left empty, code falls back to built-in defaults captured from observed traffic.
